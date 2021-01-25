b4f9f8ed8d98b2096e51dc752c6cc15bfcf7ffd8
LEE HYO JEONG <hyojeong1913@naver.com>
2021-01-26 00:10:07 +0900

    엔티티 클래스 개발

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -15,9 +15,13 @@ public class Address {
     private String street;
     private String zipcode;
 
+    // JPA 스펙 상 엔티티나 임베디드 타입은 기본 생성자를 public 또는 protected 로 설정해야 한다.
+    // public 보다는 protected 로 설정하는 것이 보다 안전
+    // JPA 가 이런 제약을 두는 이유는 JPA 구현 라이브러리가 객체를 생성할 때 리플렉션 같은 기술을 사용할 수 있도록 지원해야 하기 때문이다.
     protected Address() {
     }
 
+    // 생성자에서 값을 모두 초기화하여 변경 불가능한 클래스 생성
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -21,6 +21,9 @@ public class Category {
 
     private String name;
 
+    // 실무에서는 가급적 ManyToMany 사용하지 않는 것이 좋다.
+    // 중간 테이블(예: category_item)에 컬럼을 추가할 수 없고, 세밀하게 쿼리를 실행하기 어렵기 때문에 실무에서 사용하기에는 한계가 있다.
+    // 그러므로 중간 엔티티(예: category_item)를 만들어 대다대 매핑을 일대다, 다대일 매핑으로 풀어내서 사용하는 것이 좋다.
     @ManyToMany
     @JoinTable(name = "category_item",
             joinColumns = @JoinColumn(name = "category_id"),
