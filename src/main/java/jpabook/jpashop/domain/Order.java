e6ee5501452cddd464d6b6fb2de8250c7c33f823
LEE HYO JEONG <hyojeong1913@naver.com>
2021-02-04 00:10:08 +0900

    주문, 주문 상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -38,21 +38,93 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCLE]
 
-    // 양방향 연관 관계 메서드
+    /**
+     * 양방향 연관 관계 메서드
+     * @param member
+     */
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
     }
 
-    // 양방향 연관 관계 메서드
+    /**
+     * 양방향 연관 관계 메서드
+     * @param orderItem
+     */
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
-    // 양방향 연관 관계 메서드
+    /**
+     * 양방향 연관 관계 메서드
+     * @param delivery
+     */
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    /**
+     * 생성 메서드
+     * 주문 엔티티를 생성할 때 사용
+     * 주문 회원, 배송 정보, 주문 상품의 정보를 받아서 실제 주문 엔티티를 생성
+     * @param member
+     * @param delivery
+     * @param orderItems
+     * @return
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    /**
+     * 비즈니스 로직
+     * 주문 취소 시 사용
+     */
+    public void cancel() {
+        // 만약 이미 배송을 완료한 상품이면 주문을 취소하지 못하도록 예외를 발생시킨다.
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 주문 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCLE);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * 조회 로직
+     * 전체 주문 가격 조회
+     * 전체 주문 가격을 알려면 각각의 주문 상품 가격을 알아야 한다.
+     * 연관된 주문 상품들의 가격을 조회해서 더한 값을 반환
+     * 실무에서는 주로 주문에 전체 주문 가격 필드를 두고 역정규화 한다.
+     * @return
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+
+        // 위 내용을 한줄로 요약 가능
+//        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
     }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -29,4 +29,43 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+    /**
+     * 생성 메서드
+     * 주문 상품, 가격, 수량 정보를 사용해서 주문 상품 엔티티를 생성
+     * @param item
+     * @param orderPrice
+     * @param count
+     * @return
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+
+         orderItem.setItem(item);
+         orderItem.setOrderPrice(orderPrice);
+         orderItem.setCount(count);
+
+         // 주문한 수량만큼 상품의 재고를 줄인다.
+         item.removeStock(count);
+
+         return orderItem;
+    }
+
+    /**
+     * 비즈니스 로직
+     * 주문 취소 시 취소한 주문 수량만큼 상품의 재고를 증가시킨다.
+     */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    /**
+     * 조회 로직
+     * 주문 상품 전체 가격 조회
+     * 주문 가격에 수량을 곱한 값 반환
+     * @return
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
