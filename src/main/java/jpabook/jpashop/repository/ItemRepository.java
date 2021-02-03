033c6ec00ab9f60a107129629bf58043f2d67c37
LEE HYO JEONG <hyojeong1913@naver.com>
2021-02-03 23:28:03 +0900

    상품 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        // id 가 없는 경우 신규로 가정
+        if (item.getId() == null) {
+            em.persist(item);
+        } else { // id 가 있는 경우 DB 에 저장된 엔티티를 수정한다고 가정
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("SELECT i FROM Item i", Item.class).getResultList();
+    }
+}
